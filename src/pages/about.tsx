b73fb69f637b34778b742972df3ff855a5a447ea
Iram-Tech <75712443+Iram-Tech@users.noreply.github.com>
2023-05-30 15:38:56 -0700

    Updated index page and added an About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Page</h1>
+            <p>Iram is the creator of this About Page</p>
+        </main>
+    )
+}
+
+export default AboutPage
