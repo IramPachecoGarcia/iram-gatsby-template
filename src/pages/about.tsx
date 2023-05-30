5f35794fda885a4bc6e5ae3dff8bdf5d44d6142f
Iram-Tech <75712443+Iram-Tech@users.noreply.github.com>
2023-05-30 16:09:43 -0700

    Add component file and directory. Also, added Layout component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return (
         <main>
             <h1>About Page</h1>
+            <Link to="/">Back to home</Link>
             <p>Iram is the creator of this About Page</p>
         </main>
     )
 }
 
+export const Head = () => <title>About Me</title>
 export default AboutPage
